ae70b93e7799b2aa5d29256e454f518f5ac43f04
TaiNguyen <nguyentai0371@gmail.com>
2023-09-16 22:00:12 +0700

    event detail

--- src/components/customs/Tabs/Tabs.tsx
+++ src/components/customs/Tabs/Tabs.tsx
@@ -9,23 +9,27 @@ const Tabs = ({ tabHeader, tabConent, className }: TabsProp) => {
   return (
     <div className={`${className}`}>
       <div>
-        <ul className='relative flex shadow-border-full rounded-[10px] h-12'>
+        <ul className="relative flex h-12 rounded-[10px] shadow-border-full">
           {tabHeader.map((header: string, index: number) => (
             <li
               key={index}
               onClick={() => setTabIndex(index)}
-              className={`w-[${100 / tabHeader.length}%] flex items-center justify-center rounded-[10px] ${tabIndex === index ? 'text-white' : ''} z-10`}
+              className={`w-[${100 / tabHeader.length}%] flex items-center justify-center rounded-[10px] ${
+                tabIndex === index ? 'text-white' : ''
+              } z-10`}
             >
               {header}
             </li>
           ))}
           <li
-              className={`transition-all translate-x-[${tabIndex*100}%] bg-cs_purple absolute h-full w-[${100 / tabHeader.length}%] flex items-center justify-center rounded-[10px]`}
-            >
-            </li>
+            style={{ transform: `translateX(${tabIndex * 100}%)` }}
+            className={`absolute h-full bg-cs_purple transition-all w-[${
+              100 / tabHeader.length
+            }%] flex items-center justify-center rounded-[10px]`}
+          ></li>
         </ul>
       </div>
-      <div className='p-3'>{tabConent.find((content, index) => index === tabIndex)}</div>
+      <div className="p-3">{tabConent.find((content, index) => index === tabIndex)}</div>
     </div>
   );
 };
