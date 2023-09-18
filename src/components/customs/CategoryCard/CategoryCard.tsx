4fd6b5bf0fd4cbf40ce36e30f6d2b707378198b2
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2023-09-18 22:31:01 +0700

    ~ update layout

--- src/components/customs/CategoryCard/CategoryCard.tsx
+++ src/components/customs/CategoryCard/CategoryCard.tsx
@@ -8,10 +8,10 @@ type CategoryCardProps = {
 };
 const CategoryCard = ({ id, image, link, name, className }: CategoryCardProps) => {
   return (
-    <div className={`relative w-full overflow-hidden rounded-xl transition hover:-translate-y-5 ${className}`} key={id}>
+    <div className={`relative w-full z-10 overflow-hidden rounded-xl transition  ${className}`} key={id}>
       <Link to={link}>
         <div className="absolute z-30 h-60 w-full rounded-xl bg-cs_purple opacity-50"></div>
-        <img src={image} alt="cate" className="h-60 w-full rounded-xl object-cover" />
+        <img src={image}  alt="cate" className="h-60 w-full hover:scale-110 rounded-xl object-cover" />
         <p className="absolute bottom-5 left-5 z-50 text-xl font-bold text-white">{name}</p>
       </Link>
     </div>
