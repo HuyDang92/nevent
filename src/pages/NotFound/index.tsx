b8e3589eb28c997796906c579c2d4ebcce2b468f
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-10-08 12:19:57 +0700

    update not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,53 +1,25 @@
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import bg from 'assets/images/background.png';
-import symbol from 'assets/images/symbol.png';
-// import logo from 'assets/logo/logo-fpt.png';
+import { NavLink } from 'react-router-dom';
+import { useRive } from 'rive-react';
+import { notFound } from '~/assets/rivs';
+import View from '~/motion/View';
 
 function NotFound() {
-  // const navigate = useNavigate();
+
+  const { rive, RiveComponent } = useRive({
+    src: notFound,
+    autoplay: true,
+    stateMachines: "State Machine 1",
+  })
 
   return (
-    <>
-      <img src={bg} alt="" className="fixed  h-[100vh] w-[100vw] object-cover" />
-      <section className="flex min-h-screen items-center justify-center dark:bg-gray-900">
-        <div className="relative">
-          <div className="mx-auto max-w-screen-sm text-center">
-            <h1 className="dark:text-primary-500 mb-8 text-6xl font-black  tracking-widest text-cs_dark">404</h1>
-            <img src={symbol} alt="" />
-            <p className="mb-2 text-2xl font-bold tracking-tight text-cs_dark ">
-              Trang bạn truy cập không tồn tại.
-            </p>
-            <p className="text-md mb-4 font-light text-gray-500 dark:text-gray-400">Vui lòng kiểm tra lại.</p>
-            <Link
-              to={'/'}
-              className="focus:ring-primary-300 dark:focus:ring-primary-900 my-4 inline-flex rounded-lg bg-cs_dark px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-            >
-              Quay lại trang chủ
-            </Link>
-          </div>
-        </div>
-      </section>
-      {/* <section className="fixed mx-auto h-[100vh] bg-cs_dark px-10 pt-5">
-        <img src={logo} alt="" className="mx-auto w-40" />
-        <h1 class="my-2 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-center text-3xl font-extrabold text-transparent">
-          Hiện website chưa hỗ trợ cho phiên bản cho màn hình tablet. Vui lòng
-          truy cập trên máy tính có trải nghiệm tốt nhất.
-          <br />
-        </h1>
-        <h1 className="my-6 text-center text-3xl">😊😊😊</h1>
-        <iframe
-          className="mx-auto rounded-2xl"
-          width="700"
-          height="400"
-          src="https://www.youtube.com/embed/yI3dEh0AvxE"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </section> */}
-    </>
+    <View className='h-screen bg-[#0f0f0f] flex  flex-col justify-center items-center relative' >
+      <View className='flex flex-col items-center text-white mt-8'>
+        <h1 className='font-bold text-[6rem]'>404</h1>
+        <p>Opp! Có vẻ như trang này không tồn tại.</p>
+      </View>
+      <RiveComponent width={'100%'} height={'100%'} />
+      <NavLink to='/' className=' mb-4 py-4 px-8 rounded-lg bg-white text-black lg:bottom-10'>Cút về trang chủ</NavLink>
+    </View>
   );
 }
 
