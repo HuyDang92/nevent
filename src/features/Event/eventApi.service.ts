4a8cc6307023c532b23c994fa6a077e651c80ccf
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2023-11-04 18:18:35 +0700

    ~ update

--- src/features/Event/eventApi.service.ts
+++ src/features/Event/eventApi.service.ts
@@ -34,10 +34,12 @@ export const eventApi = createApi({
     }),
 
     getAllEvent: builder.query<any, any>({
-      query: ({ page, limit, search, status }) =>
+      query: ({ page, limit, search, status, categories, hotLevel, start_date, location }) =>
         `/api/events/get-all?page=${page}&limit=${limit}${search ? '&search=' + search : ''}${
           status ? '&status=' + status : ''
-        }`,
+        }${categories ? '&categories=' + categories : ''}${hotLevel ? '&hotLevel=' + hotLevel : ''}${
+          start_date ? '&start_date=' + start_date : ''
+        }${location ? '&location=' + location : ''}`,
     }),
     getEventById: builder.query<any, string>({
       query: (eventId) => `/api/events/detail/${eventId}`,
