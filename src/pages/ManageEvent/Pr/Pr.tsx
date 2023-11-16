37a129a95f095103cd66371d1bfe55ead7269bf5
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2023-11-16 14:07:51 +0700

    ~ update

--- src/pages/ManageEvent/Pr/Pr.tsx
+++ src/pages/ManageEvent/Pr/Pr.tsx
@@ -3,16 +3,18 @@ import { Link, useParams } from 'react-router-dom';
 import Dropdown from '~/components/Dropdown';
 import Icon from '~/components/customs/Icon';
 import { useGetEventByIdQuery } from '~/features/Event/eventApi.service';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useDebounce } from '~/hooks/useDebounce';
 import Input from '~/components/customs/Input';
 const Pr = () => {
   const { idEvent } = useParams();
   const event = useGetEventByIdQuery(idEvent || '');
-  const [widthIpt, setWidthIpt] = useState('0px');
-  const [heightIpt, setHeightIpt] = useState('0px');
+  const [widthIpt, setWidthIpt] = useState<string>('0px');
+  const [heightIpt, setHeightIpt] = useState<string>('0px');
+
   const width = useDebounce(widthIpt, 500).searchValue;
   const height = useDebounce(heightIpt, 500).searchValue;
+
   return (
     <div className="h-full w-full rounded-2xl bg-cs_light p-7 dark:bg-cs_lightDark">
       <div className="flex justify-between">
@@ -30,27 +32,27 @@ const Pr = () => {
               https://www.nevent.io.vn/event-detail/{idEvent}
             </Link>
             <br />
-            <Button className="my-5 !bg-[#3547E4] px-32 text-xl text-white" value="Chia sẻ lên Facebook" />
+            <Button className="my-5 !bg-[#3547E4] px-32 text-lg text-white" value="Chia sẻ lên Facebook" />
           </div>
           <div className="flex gap-5 border-b-[1px] py-5">
             <div className="w-1/2">
-              <b className="text-xl dark:text-cs_light">Chia sẻ lên Blog website</b>
+              <b className="text-lg dark:text-cs_light">Chia sẻ lên Blog website</b>
               <br />
               <span className="text-cs_grayText">
                 Copy đoạn mã bên dưới để tạo widget bán vé trên blog hoặc website của bạn
               </span>
-              <div className="flex justify-between gap-12">
+              <div className="mt-5 flex flex-col gap-5">
                 <div className="flex items-center gap-5">
                   <Input classNameInput="w-[100px]" value={widthIpt} onChange={(e) => setWidthIpt(e.target.value)} />
                   <b>X</b>
                   <Input classNameInput="w-[100px]" value={heightIpt} onChange={(e) => setHeightIpt(e.target.value)} />
                 </div>
-                <div className="w-[60%] rounded-lg bg-cs_gray p-2.5">
+                <div className="w-[90%] rounded-lg bg-[#eee] p-2.5">
                   {`<iframe width="${width}" height="${height}" style="border-radius:8px" src="nevent.io.vn/pr/${idEvent}"></iframe>`}
                 </div>
               </div>
             </div>
-            <div className="flex w-1/2 flex-col items-center rounded-lg border-[1px] border-cs_semi_green px-7 py-9">
+            <div className="flex w-1/2 flex-col items-center rounded-lg border-[1px] border-cs_semi_green py-6">
               <iframe className="rounded-lg" src={`/pr/${idEvent}`} height={height} width={width}></iframe>
             </div>
           </div>
