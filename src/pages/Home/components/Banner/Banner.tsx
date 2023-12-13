afd3a3cc54e0967ffa5677fe5f767955f367aade
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2023-12-13 16:20:37 +0700

    ~ upadte

--- src/pages/Home/components/Banner/Banner.tsx
+++ src/pages/Home/components/Banner/Banner.tsx
@@ -76,47 +76,49 @@ const Banner: React.FC<IProps> = ({ data }) => {
     <View className="relative h-[180px] rounded-[15px] bg-white md:h-[300px] lg:mb-16 lg:h-[350px] xl:h-[60vh]">
       <AnimatePresence initial={false} custom={direction}>
         <View className=" absolute h-full w-full overflow-x-hidden rounded-xl ">
-          <View className="relative h-full w-full">
-            <motion.img
-              className=" h-full w-full cursor-pointer rounded-xl object-cover "
-              key={page}
-              src={`${data[imageIndex]?.event?.banner[0].url}`}
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                x: { type: 'spring', stiffness: 300, damping: 30 },
-                opacity: { duration: 0.2 },
-              }}
-              drag="x"
-              dragConstraints={{ left: 0, right: 0 }}
-              dragElastic={1}
-              onDragEnd={(e, { offset, velocity }) => {
-                const swipe = swipePower(offset.x, velocity.x);
+          <Link to={`/event-detail/${data[imageIndex]?.event?._id}`}>
+            <View className="relative h-full w-full">
+              <motion.img
+                className=" h-full w-full cursor-pointer rounded-xl object-cover "
+                key={page}
+                src={`${data[imageIndex]?.event?.banner[0].url}`}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  x: { type: 'spring', stiffness: 300, damping: 30 },
+                  opacity: { duration: 0.2 },
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                  const swipe = swipePower(offset.x, velocity.x);
 
-                if (swipe < -swipeConfidenceThreshold) {
-                  paginate(1);
-                } else if (swipe > swipeConfidenceThreshold) {
-                  paginate(-1);
-                }
-              }}
-            />
-            <View className="absolute left-0 top-0 hidden h-full w-full bg-gradient-to-r from-[#0000008f] p-8 text-white duration-150 lg:block ">
-              <h1 className="max-w-[50rem] text-2xl font-bold">{data[imageIndex]?.event?.title}</h1>
-              <p className="text-sm">{moment(data[imageIndex]?.event?.start_date).format('hh:mm - DD/MM/YYYY')}</p>
-              <Link to={`/event-detail/${data[imageIndex]?.event?._id}`}>
-                <motion.button
-                  className="mt-8 rounded-lg bg-[#ffffffa9] px-6 py-2 text-sm font-semibold text-[#242424]"
-                  whileTap={{ scale: 0.9 }}
-                  whileHover={{ scale: 1.1 }}
-                >
-                  Xem chi tiết
-                </motion.button>
-              </Link>
+                  if (swipe < -swipeConfidenceThreshold) {
+                    paginate(1);
+                  } else if (swipe > swipeConfidenceThreshold) {
+                    paginate(-1);
+                  }
+                }}
+              />
+              <View className="absolute left-0 top-0 hidden h-full w-full bg-gradient-to-r from-[#0000008f] p-8 text-white duration-150 lg:block ">
+                <h1 className="max-w-[50rem] text-2xl font-bold">{data[imageIndex]?.event?.title}</h1>
+                <p className="text-sm">{moment(data[imageIndex]?.event?.start_date).format('hh:mm - DD/MM/YYYY')}</p>
+                <Link to={`/event-detail/${data[imageIndex]?.event?._id}`}>
+                  <motion.button
+                    className="mt-8 rounded-lg bg-[#ffffffa9] px-6 py-2 text-sm font-semibold text-[#242424]"
+                    whileTap={{ scale: 0.9 }}
+                    whileHover={{ scale: 1.1 }}
+                  >
+                    Xem chi tiết
+                  </motion.button>
+                </Link>
+              </View>
             </View>
-          </View>
+          </Link>
         </View>
       </AnimatePresence>
       <View className=" absolute bottom-0 right-0 z-10 hidden translate-y-[70%] lg:block xl:right-[30px]">
